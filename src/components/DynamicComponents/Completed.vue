da16c571e8f37bcd7371fe67a30a4e66a0a13125
vinraelll <53097647+vinraelll@users.noreply.github.com>
2022-01-15 17:09:48 +0200

    making changings with items logic

--- src/components/DynamicComponents/Completed.vue
+++ src/components/DynamicComponents/Completed.vue
@@ -3,13 +3,32 @@
     
   >
     <TodoItem
-      
+      v-for="completed in allPosts"
+      :key="completed.id"
     />
   </ul>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem.vue'
+import Button from '@/components/UI/Button.vue'
+import Icon from "@/components/icomoon/Icomoon.vue";
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+
 export default {
-  name: 'Completed',
+  components: {
+    TodoItem,
+    Button,
+    Icon
+  },
+  methods: {
+    ...mapActions(['fetchPosts']),
+    ...mapMutations(['deletePost', 'editTodo', 'saveTodo']),
+  },  
+  computed: mapGetters(['allPosts']),
+  async mounted() {
+    this.fetchPosts()
+    
+  }
 }
 </script>
